b2674f340e4c642ce1b4f1c5b0d4ae767d5e7a13
icohervas <icohervas@hotmail.com>
2018-04-17 23:15:13 +0200

    Add files via upload

---
+++ Constantes.java
@@ -0,0 +1,45 @@
+package Juego;
+
+import java.awt.Font;
+
+import Juego.herramientas.CargadorRecursos;
+
+public class Constantes {
+
+	public static final int LADO_SPRITE = 32;
+
+	public static int ANCHO_JUEGO = 1024;
+	public static int ALTO_JUEGO = 768;
+
+	// public static int ANCHO_PANTALLA_COMPLETA = 1920;
+	// public static int ALTO_PANTALLA_COMPLETA = 1080;
+
+	public static int ANCHO_PANTALLA_COMPLETA = 1024;
+	public static int ALTO_PANTALLA_COMPLETA = 768;
+
+	// 4:3
+
+	public static double FACTOR_ESCALADO_X = (double) ANCHO_PANTALLA_COMPLETA / (double) ANCHO_JUEGO;
+	public static double FACTOR_ESCALADO_Y = (double) ALTO_PANTALLA_COMPLETA / (double) ALTO_JUEGO;
+
+	public static int CENTRO_VENTANA_X = ANCHO_JUEGO / 2;
+	public static int CENTRO_VENTANA_Y = ALTO_JUEGO / 2;
+
+	public static int MARGEN_X = ANCHO_JUEGO / 2 - LADO_SPRITE / 2;
+	public static int MARGEN_Y = ALTO_JUEGO / 2 - LADO_SPRITE / 2;
+
+	public static String RUTA_MAPA = "/mapas/03.adm";
+	public static String RUTA_ICONO_RATON = "/imagenes/iconos/iconoCursor.png";
+	public static String RUTA_PERSONAJE = "/imagenes/hojasPersonajes/2.png";
+	public static String RUTA_PERSONAJE_PISTOLA = "/imagenes/hojasPersonajes/4.png";
+	public static String RUTA_ICONO_VENTANA = "/imagenes/iconos/iconoVentana.png";
+	public static String RUTA_LOGOTIPO = "/imagenes/iconos/logotipo.png";
+	public static String RUTA_OBJETOS = "/imagenes/hojasObjetos/1.png";
+	public static String RUTA_OBJETOS_ARMAS = "/imagenes/hojasObjetos/armas.png";
+	public static String RUTA_ENEMIGOS = "/imagenes/hojasEnemigos/";
+	public static String RUTA_OBJETOS_CURATIVOS = "/imagenes/hojasObjetos/1.png";
+	public static String RUTA_ARMAS = "/imagenes/hojasObjetos/armas.png";
+        public static String RUTA_ARMA_ESCOPETA = "/imagenes/hojasObjetos/escopeta.png";
+
+	public static Font FUENTE_PIXEL = CargadorRecursos.cargarFuente("/fuentes/px10.ttf");
+}
